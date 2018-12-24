1d615bd8d53b1c2882c44a1e268f23b657ac6419
taka2018 <taka2018@taka2018nomac-mini.local>
2018-12-24 12:30:17 +0900

    Initial Commit

---
+++ CocoaGraphics1224/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaGraphics1224
+//
+//  Created by taka2018 on 2018/12/24.
+//  Copyright © 2018 taka2018. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
